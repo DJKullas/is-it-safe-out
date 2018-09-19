dca19c8db38a8be0c9d77fcaff5605e40fe97e79
DJ Kullas <kullas.d@husky.neu.edu>
2018-09-19 01:08:13 -0400

    added secret file

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,19 +1,18 @@
+const config = require('../../src/secret.config.js');
 const express = require('express');
 const router = express.Router();
 
 // declare axios for making http requests
 const axios = require('axios');
-const API = 'https://api.openweathermap.org/data/2.5/weather?APPID=b090401a8d93fe8d9b1c0c9315ac27bf&units=imperial&zip=';
+const API = `https://api.openweathermap.org/data/2.5/weather?APPID=${config.weatherApiKey}&units=imperial&zip=`;
 
 /* GET api listing. */
 router.get('/', (req, res) => {
   res.send('api works');
 });
 
-// Get all posts
 router.get('/weather', (req, res) => {
-  // Get posts from the mock api
-  // This should ideally be replaced with a service that connects to MongoDB
+  console.log(config);
   axios.get(`${API}${req.query.zipCode},us`)
     .then(weather => {
         res.status(200).json(weather.data);
